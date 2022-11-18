e3b96052b076c1686873f1580e76b97a441dd2da
Mila <milasp@list.ru>
2022-11-19 01:48:17 +0300

    add logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] InputArray = { "hello", "2", "world", "333" };
+
+            string[] OutputArray = new string[InputArray.Length];
+
+            for (int i = 0; i < InputArray.Length; i++)
+            {
+                if (InputArray[i].Length <= 3)
+                {
+                    OutputArray[i] = InputArray[i];
+                }
+            }
+
+            Console.WriteLine(OutputArray);
+            for (int i = 0; i < OutputArray.Length; i++)
+            {
+                Console.Write(OutputArray[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
